8491d6eaebc4f2f09557b06119c00095ff989fd3
sinsky <wesky93@gmail.com>
2016-08-05 17:04:11 +0900

    업로드된 엑셀 파일을 원하는 데로 조작 가능한 수준까지 구현
    현재 구현된 부분
    - 업로드 파일 형식 검증
    - 시트명과 머리열 이름으로 식물 목록 열을 추출
    - 업로드된 엑셀 시트의 열에 정보가 담긴 열을 추가하고 저장

--- namechecker/forms.py
+++ namechecker/forms.py
@@ -2,3 +2,6 @@ from django import forms
 
 class ImportFile(forms.Form):
     file = forms.FileField(label='파일 선택하기', help_text='식물명을 찾을 파일을 업로드 하세')
+    sheetname = forms.CharField(max_length=20,label='시트 이름', help_text='식물 목록이 있는 시트명을 입력하세요')
+    rowname = forms.CharField(max_length=20,label='머리열 이름',help_text='식물 목록이 있는 열의 머리글 이름을 입력하세')
+    rownum = forms.IntegerField(max_value=4,label='머리열 번호',help_text='시트의 첫번째 열이 머리열이 아닌경우 머리열 번호를 입력하세요', required=False)
--- namechecker/views.py
+++ namechecker/views.py
@@ -1,3 +1,4 @@
+import openpyxl
 from django.views.generic import View
 from django.template.loader import render_to_string
 from django.http import HttpResponse, HttpResponseRedirect
@@ -6,10 +7,42 @@ import os
 from .forms import ImportFile
 import tempfile
 import openpyxl
+from openpyxl.utils import get_column_letter, column_index_from_string
+
+class Checker:
+
+    def setdata(self,file,sheet_name,row_name,row_num):
+        """ 엑셀의 속성값으로 구조 파악"""
+
+        def findtarget(head_col,row_name):
+            """ 식물 이름 목록이 있는 열 위치값을 반환 """
+            for col in head_col:
+                if col.value == row_name:
+                    return {'index':col.col_idx , 'str':col.column }
+
+        self.file = openpyxl.load_workbook(file)
+        self.sheet = self.file.get_sheet_by_name(sheet_name)
+        self.row_name = row_name
+        self.head_col = self.sheet.rows[row_num]
+        self.head_row = findtarget(self.head_col,row_name)
+        self.count_list = self.sheet.max_row
+
+        # 매칭된 이름을 저장할 셀의 위치
+        self._indx = self.sheet.max_column
+        self.row_nameko = get_column_letter(self._indx+1)
+        self.row_nameen = get_column_letter(self._indx+2)
+
+    def search(self):
+        for i in range(1,self.count_list):
+            name = self.sheet.cell(row=i,column=self.head_row['index']).value
+            self.sheet['{0}{1}'.format(self.row_nameko,i)] = name
+            self.sheet['{0}{1}'.format(self.row_nameen,i)] = name
+
+
 
 def checkname(file):
     """
-    업로드 받은 파일을 임시파일로 저장후 작업 가능 여부를 확인하고 가능할경우 수정된 엑셀 파일을 반환한다.
+    이름을 매칭하여 알려준다
     :param file:
     :return:
     """
@@ -28,8 +61,15 @@ class Index(View):
         form = ImportFile(request.POST,request.FILES)
         test = 'filecheck'
         if form.is_valid(): # 파일이 업로드 되었을 경우
-            # 업로드한 파일 호출
-            file = request.FILES['file']
+            # 업로드한 파일및 엑셀 속성값 호출
+            file = request.FILES['file']    # 업로드된 파일
+            sheet_name = request.POST['sheetname'] # 식물 목록이 존해하는 시트이름
+            row_name = request.POST['rowname'] # 식물 목록의 머릿행 이름
+            # 머릿행의 번호
+            if request.POST['rownum']:
+               row_num = int(request.POST['rownum'])-1
+            else:
+                row_num = 0
 
             # 파일 형식확인 : 엑셀파일이 아닐경우
             if file.content_type != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
@@ -41,11 +81,19 @@ class Index(View):
                 # tmp = tempfile.NamedTemporaryFile()
                 # tmp.write(file.read())
                 # 엑셀 파일을 불러 시트 목록 출력하기
-                wb = openpyxl.load_workbook(file)
-                test = wb.get_sheet_names()
+                try:
+                    target = Checker()
+                    target.setdata(file,sheet_name,row_name,row_num)
+                    target.search()
+                    target.file.save('test.xlsx')
+                    upload = 'yes'
+                    message = '파일이 성공적으로 업로드 되었습니다. 잠시만 기다리시면 작업이 완료된 파일을 다운 받을수 있습니다.'
+                    # test code
+                    message = '{0},{1},{2}'.format(target.sheet,target.head_col,target.head_row)
+                except:
+                    upload = 'no'
+                    message = '지원하지 않는 엑셀 구조 입니다.관리자 에게 문의해 주세요'
 
-                upload = 'yes'
-                message = '파일이 성공적으로 업로드 되었습니다. 잠시만 기다리시면 작업이 완료된 파일을 다운 받을수 있습니다.'
 
         else:   # 업로드 파일이 없을 경우
             upload = 'no'
