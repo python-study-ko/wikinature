28129c4d4181a47a48a89776de988f687ab1e89b
sinsky <wesky93@gmail.com>
2016-08-05 13:03:40 +0900

    파일이 업로드되는 상황에 따라 에러상황 분기

--- namechecker/views.py
+++ namechecker/views.py
@@ -16,16 +16,25 @@ class Index(View):
 
     def post(self, request):
         form = ImportFile(request.POST,request.FILES)
-        if form.is_valid():
+        test = 'filecheck'
+        if form.is_valid(): # 파일이 업로드 되었을 경우
             # 업로드한 파일 호출
             file = request.FILES['file']
 
-            # 파일 검사
+            # 파일 형식확인 : 엑셀파일이 아닐경우
             if file.content_type != 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet':
-                return HttpResponseRedirect('/check/')
+                upload = 'no'
+                test = file.content_type
+                message = '엑셀파일이 아닌거 같습니다. 파일 확장자를 확인해주시고 업로드 하세요'
+            else:
+                upload = 'yes'
+                test = file.content_type
+                message = '파일이 성공적으로 업로드 되었습니다. 잠시만 기다리시면 작업이 완료된 파일을 다운 받을수 있습니다.'
 
-            context = {'test': file.content_type, 'upload': 'yes'}
-            data = render_to_string('check/index.html', context, request=request)
-            return HttpResponse(data)
-        else:
-            return HttpResponseRedirect('/check/')
+        else:   # 업로드 파일이 없을 경우
+            upload = 'no'
+            message = '이런, 파일을 올리신게 맞나요? 저는 아무것도 받지 못했습니다.. 다시 확인해 주세'
+        form = ImportFile()
+        context = {'test': test, 'upload': upload, 'message': message, 'form':form}
+        data = render_to_string('check/index.html', context, request=request)
+        return HttpResponse(data)
