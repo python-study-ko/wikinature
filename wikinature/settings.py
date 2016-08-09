ede638b863d9bb21574a8081e4bc2412ff7e26e4
wesky93 <wesky93@gmail.com>
2016-08-09 17:28:11 +0900

    템플릿 분리 작업 1 - 메인 템플릿 제작
    부트스트랩4가 적용된 메인템플릿과
    cover 테마를 적용한 템플릿 파일 제작
    추후 cover테마를 이용한 템플릿 제작시 cover_sample파일을 복사하면됨

--- wikinature/settings.py
+++ wikinature/settings.py
@@ -139,6 +139,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 STATIC_ROOT = os.path.join(BASE_DIR, 'collect_static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
